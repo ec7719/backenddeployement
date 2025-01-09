34ccb7f545fdf9f2b2dd48bcd27f049b00f4dc52
E.Christo Raymonde <101086026+ec7719@users.noreply.github.com>
2025-01-09 21:49:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB
 # Configure CORS
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', 'https://master.dl76w5z8gkkdv.amplifyapp.com/')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
     return response
